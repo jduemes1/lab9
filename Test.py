18b08305f2bcfb2c04f7037551109e990c063ad3
jduemes1 <33322327+jduemes1@users.noreply.github.com>
2017-11-03 10:18:11 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,18 @@
-something
+import Rectangle
+
+class surface:
+    def__init__(self,"image.png",x,y,width,height):
+        self.rect = Rectangle.rectangle(x,y,height,width)
+        self.image = "image.png"
+        
+def getRect(self):
+    return self.rect
+
+class rectangle:
+    def__init__(self,x,y,height,width):
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+    def__str__(self):
+        return "x:",self.x, "y:", self.y, "height:", self.height, "width:", self.width
